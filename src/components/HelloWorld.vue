5d16a9054a60ff9adc81283fc362a2ab104af439
Hoshi <hoshimure.47@gmail.com>
2021-04-29 22:41:41 +0900

    :+1: 1on1 Connection

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,19 @@
 <template>
   <h2>Hello Vue.js with Vite.js</h2>
+  <video @loadeddata="onLoadedData" autoplay :srcObject.prop="stream"></video>
   <video
     @loadeddata="onLoadedData"
-    ref="videoRef"
     autoplay
-    :srcObject.prop="stream"
+    :srcObject.prop="anotherStream"
   ></video>
+  <p>My Peer ID: {{ id }}</p>
+  <input type="text" v-model="state.text" />
+  <button @click="onCall">Call</button>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent, onMounted } from "vue";
-import Peer from "skyway-js";
+import { ref, defineComponent, onMounted, reactive } from "vue";
+import Peer, { MediaConnection } from "skyway-js";
 
 export default defineComponent({
   name: "HelloWorld",
@@ -21,21 +24,51 @@ export default defineComponent({
     },
   },
   setup: () => {
-    const videoRef = ref<HTMLMediaElement | null>(null);
-    const stream = ref<MediaStream | null>(null);
+    const peer = new Peer({
+      key: "f6a377a0-a4e4-473e-9d8a-a490802fee85",
+      debug: 3,
+    });
+
+    const id = ref();
+    const stream = ref<MediaStream | undefined>();
+    const anotherStream = ref<MediaStream | undefined>();
+
+    const state = reactive({
+      text: "",
+    });
 
     onMounted(async () => {
-      const peer = new Peer({ key: "f6a377a0-a4e4-473e-9d8a-a490802fee85" });
+      peer.on("open", () => {
+        id.value = peer.id;
+      });
 
-      // カメラ映像取得
-      stream.value = await navigator.mediaDevices.getUserMedia({ video: true });
+      const s = await navigator.mediaDevices.getUserMedia({ video: true });
+      stream.value = s;
     });
 
     const onLoadedData = () => {
       console.info("Loaded");
     };
 
-    return { videoRef, stream, onLoadedData };
+    const setEventListener = (connection: MediaConnection) => {
+      connection.on("stream", (s) => {
+        anotherStream.value = s;
+      });
+    };
+
+    peer.on("call", (connection: MediaConnection) => {
+      connection.answer(stream.value);
+      setEventListener(connection);
+    });
+
+    const onCall = () => {
+      console.warn(state.text);
+      const targetPeerId = state.text;
+      const mediaConnection = peer.call(targetPeerId, stream.value);
+      setEventListener(mediaConnection);
+    };
+
+    return { id, stream, anotherStream, onLoadedData, onCall, state };
   },
 });
 </script>
